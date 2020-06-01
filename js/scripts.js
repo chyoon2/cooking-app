31360d90faa7915050ae19ac438d8c69de2fd6ff
Jeff Kim <kim2jy@hotmail.com>
2020-06-01 11:46:50 -0700

    initial commit, create index.html, scripts.js and declare celsiusToFaranheit function
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business Logic
+
+const celsiusToFaranheit = function(celsius) {
+  result = (celsius * (5 / 9) + 32);
+};
+
+// User Interface Logic
+
+const celsius = parseInt(prompt("Enter a temperature in Celsius"));
+alert(result);
